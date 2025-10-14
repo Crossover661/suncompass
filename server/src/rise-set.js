3f7a5f173f52e739c1cb917b59462e01b6354d14
Crossover661 <sanjay.k.srikanth@gmail.com>
2025-10-14 00:05:04 -0700

    add functionality for continuous display

--- server/src/rise-set.js
+++ server/src/rise-set.js
@@ -11,14 +11,16 @@ if (args.length == 2) {
     zone = find(lat, long)[0];
     date = DateTime.now().setZone(zone);   
 }
-else if (args.length == 4) { // Accepts coordinates. Example: "node rise-set 40.75 -73.99" gives rise-set times for Manhattan, New York City in Eastern Time.
+else if (args.length == 4) { 
+    /* Accepts coordinates. Example: "node rise-set 40.75 -73.99" gives rise-set times for Manhattan, New York City in Eastern Time. */
     [lat, long] = [Number(args[2]), Number(args[3])];
     zone = find(lat, long)[0];
     date = DateTime.now().setZone(zone);
 }
 else if (args.length == 5) {
     /* Coordinates and date. Example: "node rise-set 40.75 -73.99" gives times for June 20, 2024 in New York City in EDT
-    The date argument can be replaced with "me" for March equinox, "js" for June solstice, "se" for Sep equinox, "ds" for Dec solstice
+    The date argument can be replaced with "me" for March equinox, "js" for June solstice, "se" for Sep equinox, "ds" for Dec solstice,
+    or "continuous" for a continuously-updating display.
     The specific time can also be specified in the date. Examples:
     "2022-02-22T02:22:22" --> February 22, 2022 at 2:22:22 AM
     "2024-12-25T19:12:06" --> December 25, 2024 at 7:12:06 PM (19:12:06)
@@ -30,41 +32,53 @@ else if (args.length == 5) {
     else if (args[4] == "js") {date = suncalc.junSolstice(curYear, zone);}
     else if (args[4] == "se") {date = suncalc.sepEquinox(curYear, zone);}
     else if (args[4] == "ds") {date = suncalc.decSolstice(curYear, zone);}
-    else {date = DateTime.fromISO(args[4], {zone: zone});}
+    else if (args[4] != "continuous") {date = DateTime.fromISO(args[4], {zone: zone});}
 }
 else {
     console.log("Invalid argument");
     process.exit(1);
 }
 
-// Print subsolar point, sun's apparent elevation, and subsolar point
+// Print subsolar point, sun's apparent elevation, and solar noon/midnight/sunrise/sunset/twilight times.
+function printSunInfo(lat, long, zone, date) {
+    let subsolarPoint = suncalc.subsolarPoint(date);
+    let [elev, az] = suncalc.sunPosition(lat, long, date);
+    let apparentElev = suncalc.refract(elev);
+    let dist = suncalc.sunDistance(date);
 
-let subsolarPoint = suncalc.subsolarPoint(date);
-let [elev, az] = suncalc.sunPosition(lat, long, date);
-let apparentElev = suncalc.refract(elev);
-let dist = suncalc.sunDistance(date);
+    process.stdout.write(`\r${zone}\n`);
+    process.stdout.write(`\r${date.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS)}\n`);
+    process.stdout.write(`\rCurrent sun elevation: ${elev.toFixed(4)}° (After refraction: ${apparentElev.toFixed(4)}°)\n`);
+    process.stdout.write(`\rCurrent sun bearing: ${az.toFixed(4)}° (${direction(az)})\n`);
+    process.stdout.write(`\rSubsolar point: ${subsolarPoint[0].toFixed(4)}, ${subsolarPoint[1].toFixed(4)}\n`);
+    process.stdout.write(`\rSun-earth distance: ${dist.toFixed(0)} km (${(dist/1.609344).toFixed(0)} mi)\n`);
 
-console.log(zone);
-console.log(date.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS));
-console.log("Current sun elevation: " + elev.toFixed(4) + "°" + " (After refraction: " + apparentElev.toFixed(4) + "°)");
-console.log("Current sun bearing: " + az.toFixed(4) + "° (" + direction(az) + ")");
-console.log("Subsolar point: " + subsolarPoint[0].toFixed(4) + ", " + subsolarPoint[1].toFixed(4));
-console.log("Sun-earth distance: " + dist.toFixed(0) + " km (" + (dist/1.609344).toFixed(0) + " mi)");
+    // Print day length
+    let dayLength = Math.round(suncalc.dayLength(lat, long, date));
+    if (dayLength == -1) {process.stdout.write("\rDay length: undefined\n");}
+    else {process.stdout.write(`\rDay length: ${Duration.fromObject({seconds: dayLength}).toFormat("h:mm:ss")}\n\r\n`);}
 
-// Print day length
-let dayLength = Math.round(suncalc.dayLength(lat, long, date));
-if (dayLength == -1) {console.log("Day length: undefined");}
-else {console.log("Day length: " + Duration.fromObject({seconds: dayLength}).toFormat("h:mm:ss"));}
-console.log("");
+    // Print sunrise, sunset, solar noon, solar midnight, and twilight times
+    let solarEvents = suncalc.allSunEvents(lat, long, date);
+    process.stdout.write(`\r         Event |        Time | Elevation |       Bearing\n`); // header
+    for (let event of solarEvents) {
+        let bold = false;
+        if (event.eventType == "Sunrise" || event.eventType == "Sunset" || event.eventType == "Solar Noon") {bold = true};
+        let [r, g, b] = [128, 128, 128];
+        if (event.eventType == "Sunrise" || event.eventType == "Sunset") {[r, g, b] = [255, 255, 0];}
+        else if (event.solarElevation >= -5/6) {[r, g, b] = [255, 255, 255];}
+        process.stdout.write(`\r${event.toStringFormatted(bold, r, g, b)}\n`);
+    }
 
-// Print sunrise, sunset, solar noon, solar midnight, and twilight times
-let solarEvents = suncalc.allSunEvents(lat, long, date);
-console.log("         Event |        Time | Elevation |       Bearing"); // header
-for (let event of solarEvents) {
-    let bold = false;
-    if (event.eventType == "Sunrise" || event.eventType == "Sunset" || event.eventType == "Solar Noon") {bold = true};
-    let [r, g, b] = [128, 128, 128];
-    if (event.eventType == "Sunrise" || event.eventType == "Sunset") {[r, g, b] = [255, 255, 0];}
-    else if (event.solarElevation >= -5/6) {[r, g, b] = [255, 255, 255];}
-    console.log(event.toStringFormatted(bold, r, g, b));
+    return 9 + solarEvents.length; // number of lines in output
 }
+
+function printEverySecond(lat, long, zone, lines = 0) {
+    const date = DateTime.now().setZone(zone);
+    if (lines > 0) {process.stdout.write(`\x1b[${lines}A`);}
+    const l = printSunInfo(lat, long, zone, date.set({millisecond: 0}));
+    setTimeout(printEverySecond(lat, long, zone, l), 1000 - date.millisecond); // delay until next full second
+}
+
+if (date == null) {printEverySecond(lat, long, zone);}
+else {printSunInfo(lat, long, zone, date);}
--- server/src/test.js
+++ server/src/test.js
@@ -4,24 +4,11 @@ import * as suncalc from "./suncalc.js";
 import SunTime from "./SunTime.js";
 import {direction} from "./mathfuncs.js";
 
-/*let date1 = DateTime.fromISO("2025-10-11T12:45:57.375", {zone: "America/Los_Angeles"})
-let date2 = date1.plus(2);
-let sp1 = suncalc.sunPosition(34.42, -119.85, date2)[0];
-let sp2 = suncalc.sunPosition(34.42, -119.85, date1)[0];
-let diff = sp2 - sp1;
-console.log(sp1);
-console.log(sp2);
-console.log(diff);*/
-
-let date = DateTime.fromISO("2025-06-20T00:00:00.000", {zone: "utc"});
-let newDate = date.plus(1000);
-let minElevDiff = Number.POSITIVE_INFINITY;
-let [lat, long] = [89.9999,0];
-
-while (newDate.day == date.day) {
-    let diff = suncalc.sunPosition(lat, long, newDate)[0] - suncalc.sunPosition(lat, long, date)[0];
-    if (Math.abs(diff) < minElevDiff) {minElevDiff = Math.abs(diff);}
-    date = newDate;
-    newDate = newDate.plus(1000);
+let [lat, long] = [34.42, -119.85];
+let date = DateTime.fromISO("2025-01-01T00:00:00.000", {zone: "America/Los_Angeles"});
+for (let i=1; i<=12; i++) {
+    // prints the difference between calculated solar noon and actual solar noon, in seconds
+    console.log(date.toFormat("yyyy-MM-dd") + " " + 
+        (suncalc.solarTime(long, suncalc.solarNoon(lat, long, date)[0].time) - 720)*60);
+    date = date.plus({months: 1});
 }
-console.log(minElevDiff);
